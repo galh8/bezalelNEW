0a34bbde5185890087cd7def748eacea92d29d7f
galh8 <galhuta@gmail.com>
2017-01-04 19:35:51 +0200

    optimizations

--- TripInfo.cpp
+++ TripInfo.cpp
@@ -152,3 +152,14 @@ void TripInfo::setIsAssigned(bool isAssigned) {
     TripInfo::isAssigned = isAssigned;
 }
 
+/**
+* destructor. deletes all the realated vectors.
+*/
+TripInfo::~TripInfo() {
+    int i;
+    for (i=0;i < tripRoute.size();i++) {
+        delete(tripRoute[i]);
+    }
+    delete(startingPoint);
+    delete(endingPoint);
+}
