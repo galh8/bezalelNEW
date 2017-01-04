c023148a0d8b8fdda06860162166b2e7a167f85f
galh8 <galhuta@gmail.com>
2017-01-04 19:05:23 +0200

    optimizations

--- server.cpp
+++ server.cpp
@@ -10,7 +10,6 @@ BOOST_CLASS_EXPORT_GUID(GridNode,"GridNode")
 BOOST_CLASS_EXPORT_GUID(StandardCab,"StandardCab")
 
 int main(int argc, char *argv[]) {
-    int p=1;//delete after.
     int timePassed = 0;
     int serverOperation;
     int sizeX, sizeY;
